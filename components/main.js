01dbb406b77b6f81e12831004917c7d52f5f5ab6
Leonel Lamelas <leonel.lamelas@Leonels-MBP.attlocal.net>
2023-09-03 14:18:23 -0400

    created the js components for the main section of the Js

---
+++ components/main.js
@@ -0,0 +1,129 @@
+import ToDoList from "./todolist.js";
+import ToDoItem from "./todoitem.js";
+
+const toDoList = new ToDoList();
+
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete"){
+        initApp();
+    }
+});
+
+const initApp = () => {
+    const itemEnryForm = document.getElementById("itemEntryForm");
+    itemEnryForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        processSubmission();
+    })
+
+    const clearItems = document.getElementById("clearItems");
+    clearItems.addEventListener("click", (event) => {
+        const list = toDoList.getList();
+        if(list.length) {
+            const confirmed = confirm("Do you want to clear the list?");
+            if (confirmed) {
+                toDoList.clearList();
+                updatePersistentData(toDoList.getList());
+                refreshThePage();
+            }
+        }
+    })
+
+    refreshThePage();
+}
+
+const refreshThePage = () => {
+    clearListDisplay();
+    renderList();
+    clearItemEntryField();
+    setFocusOnItemEntry();
+}
+
+const clearListDisplay = () => {
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+}
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while(child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach(item => {
+        buildListItem(item)
+    });
+};
+
+const buildListItem = (item) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    const check = document.createElement("input")
+    check.type = "checkbox";
+    check.id = item.getId();
+    check.tabIndex = 0;
+    addClickListenerToCheckBox(check);
+    const label = document.createElement("label");
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById("listItems");
+    container.appendChild(div);
+};
+
+const addClickListenerToCheckBox = (checkbox) => {
+    checkbox.addEventListener("click", (event) => {
+        toDoList.removeItemFromList(checkbox.id);
+        updatePersistentData(toDoList.getList());
+        setTimeout(() => {
+            refreshThePage();
+        }, 1000);
+    });
+};
+
+const updatePersistentData = (listArray) => {
+    localStorage.setItem("myToDoList", JSON.stringify(listArray));
+};
+
+const clearItemEntryField = () => {
+    document.getElementById("newItem").value = "";
+};
+
+const setFocusOnItemEntry = () => {
+    document.getElementById("newItem").focus();
+};
+
+const processSubmission = () => {
+    const newEntryText = getNewEntry();
+    if (!newEntryText.length) return;
+    const nextItemId = calcNextItemId();
+    const toDoItem = createNewItem(nextItemId, newEntryText);
+    toDoList.addItemToList(toDoItem);
+    updatePersistentData(toDoList.getList());
+    refreshThePage();
+}
+
+const getNewEntry = () => {
+    return document.getElementById("newItem").value.trim()
+}
+
+const calcNextItemId = () => {
+    let nextItemId = 1;
+    const list = toDoList.getList();
+    if (list.length > 0) {
+        nextItemId = list[list.length - 1].getId() + 1;
+    }
+    return nextItemId;
+};
+
+const createNewItem = (itemId, itemText) => {
+    const toDo = new ToDoItem();
+    toDo.setId(itemId);
+    toDo.setItem(itemText);
+    return toDo;
+}
